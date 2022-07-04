6afd94c44553cd4de0d452a47e142f82b7a71fc0
Luis Carlos Porras Castro <74470666+luiskacr@users.noreply.github.com>
2022-07-03 20:28:55 -0600

    Creation of Custom Order List and payment history with its implementations, Annotations in the entities' models.

---
+++ BackEnd/DAL/CustomOrdenListDALImple.cs
@@ -0,0 +1,113 @@
+﻿using BackEnd.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd.DAL
+{
+    internal class CustomOrdenListDALImple : ICustomOrdenListDAL
+    {
+        ProyectoCreditosContext context;
+
+        public CustomOrdenListDALImple() {
+            context = new ProyectoCreditosContext();
+        }
+
+        public bool Add(CustomOrderList entity)
+        {
+            try {
+                using (UnidadDeTrabajo<CustomOrderList> unidad = new UnidadDeTrabajo<CustomOrderList>(context)) { 
+                    unidad.genericDAL.Add(entity);
+                    return unidad.Complete();
+                }
+            }catch (Exception ex) {
+                return false; 
+            }
+        }
+
+        public void AddRange(IEnumerable<CustomOrderList> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CustomOrderList> Find(Expression<Func<CustomOrderList, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomOrderList Get(int id)
+        {
+            try
+            {
+                CustomOrderList customOrderList;
+                using (UnidadDeTrabajo<CustomOrderList> unidad = new UnidadDeTrabajo<CustomOrderList>(context)) {
+                    customOrderList = unidad.genericDAL.Get(id);
+                }
+                return customOrderList;
+            }
+            catch (Exception ex) {
+                throw;
+            }
+        }
+
+        public IEnumerable<CustomOrderList> GetAll()
+        {
+            try
+            {
+                IEnumerable<CustomOrderList> customOrderLists;
+                using (UnidadDeTrabajo<CustomOrderList> unidad = new UnidadDeTrabajo<CustomOrderList>(context)) { 
+                    customOrderLists = unidad.genericDAL.GetAll();
+                }
+                return customOrderLists;
+            }
+            catch (Exception ex){
+                throw;
+            }
+        }
+
+        public bool Remove(CustomOrderList entity)
+        {
+            bool result = false;
+            try
+            {
+                using (UnidadDeTrabajo<CustomOrderList> unidad = new UnidadDeTrabajo<CustomOrderList>(context)) { 
+                    unidad.genericDAL.Remove(entity);
+                    result = unidad.Complete();
+                }
+                return result;
+            }
+            catch (Exception ex) {
+                return result;
+            }
+        }
+
+        public void RemoveRange(IEnumerable<CustomOrderList> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CustomOrderList SingleOrDefault(Expression<Func<CustomOrderList, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(CustomOrderList entity)
+        {
+            bool result = false;
+            try
+            {
+                using (UnidadDeTrabajo<CustomOrderList> unidad = new UnidadDeTrabajo<CustomOrderList>(context)) {
+                    unidad.genericDAL.Update(entity);
+                    result = unidad.Complete();
+                }
+                return result;
+            }
+            catch (Exception ex) {
+                return result;
+            }
+        }
+    }
+}
---
+++ BackEnd/DAL/PaymentHistoryDALImple.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BackEnd.Entities;
+
+namespace BackEnd.DAL
+{
+    internal class PaymentHistoryDALImple : IPaymentHistoryDAL
+    {
+        ProyectoCreditosContext context;
+        public PaymentHistoryDALImple() {
+            context = new ProyectoCreditosContext();
+        }
+        public bool Add(PaymentHistory entity)
+        {
+            try {
+                using (UnidadDeTrabajo<PaymentHistory> UnidadDeTrabajo = new UnidadDeTrabajo<PaymentHistory>(context)) {
+                    UnidadDeTrabajo.genericDAL.Add(entity);
+                    return UnidadDeTrabajo.Complete();
+                }
+            }catch(Exception) { 
+                return false;
+            }
+        }
+
+        public void AddRange(IEnumerable<PaymentHistory> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PaymentHistory> Find(Expression<Func<PaymentHistory, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PaymentHistory Get(int id)
+        {
+            try
+            {
+                PaymentHistory paymentHistory;
+                using (UnidadDeTrabajo<PaymentHistory> UnidadDeTrabajo = new UnidadDeTrabajo<PaymentHistory>(context)) {
+                    paymentHistory = UnidadDeTrabajo.genericDAL.Get(id);
+                    return paymentHistory;
+                }
+            }
+            catch (Exception) {
+                throw;
+            }
+        }
+
+        public IEnumerable<PaymentHistory> GetAll()
+        {
+            try
+            {
+                IEnumerable<PaymentHistory> paymentHistories;
+                using (UnidadDeTrabajo<PaymentHistory> UnidadDeTrabajo = new UnidadDeTrabajo<PaymentHistory>(context))
+                {
+                    paymentHistories = UnidadDeTrabajo.genericDAL.GetAll();
+                }
+                return paymentHistories;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool Remove(PaymentHistory entity)
+        {
+            bool result = false;
+            try
+            {
+                using (UnidadDeTrabajo<PaymentHistory> UnidadDeTrabajo = new UnidadDeTrabajo<PaymentHistory>(context))
+                {
+                    UnidadDeTrabajo.genericDAL.Remove(entity);
+                    result = UnidadDeTrabajo.Complete();
+                }
+                return result;
+            }
+            catch (Exception) {
+                return result;
+            }
+        }
+
+        public void RemoveRange(IEnumerable<PaymentHistory> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PaymentHistory SingleOrDefault(Expression<Func<PaymentHistory, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(PaymentHistory entity)
+        {
+            bool result = false;
+            try
+            {
+                using (UnidadDeTrabajo<PaymentHistory> UnidadDeTrabajo = new UnidadDeTrabajo<PaymentHistory>(context)) { 
+                    UnidadDeTrabajo.genericDAL.Update(entity);
+                    result = UnidadDeTrabajo.Complete();
+                }
+                return result;
+            }
+            catch (Exception) {
+                return result;
+            }
+        }
+    }
+}
