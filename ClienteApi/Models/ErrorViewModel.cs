8d6ec3445dbdb815369734bf86507f3adc982ef4
Jose Andrés Espinoza <Jandres.espinoza@hotmail.com>
2022-07-26 14:49:32 -0600

    Revert "Merge branch 'BackendApi' of https://github.com/Guilderm/SC701-PrograAvanzada-CapstoneProject into BackendApi"
    This reverts commit 02c2add89925202ab27e0edeeab2b1a19858af86, reversing
    changes made to 3bf66427c5c1b415b1bd392619f9d8e3c0c905ef.

--- ClienteApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ClienteApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
