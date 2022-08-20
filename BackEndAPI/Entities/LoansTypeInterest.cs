6f02585b352d63bf93441d750799d740c7b7a3e7
Luis Carlos Porras Castro <74470666+luiskacr@users.noreply.github.com>
2022-08-20 04:24:49 -0600

    Cambios de Pagos

---
+++ BackEndAPI/Entities/LoansTypeInterest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BackEndAPI.Entities
+{
+    public partial class LoansTypeInterest
+    {
+        public int IdloansTypeInterest { get; set; }
+        public int IdloansType { get; set; }
+        public int IdCurrencies { get; set; }
+        public decimal InteresRate { get; set; }
+        public int? YearTime { get; set; }
+
+        public virtual Currency IdCurrenciesNavigation { get; set; } = null!;
+        public virtual LoansType IdloansTypeNavigation { get; set; } = null!;
+    }
+}
